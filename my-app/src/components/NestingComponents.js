a176404d097032184cbc04d6af1072aa0812b48e
Christoph33one <cjbrowning23@hotmail.com>
2023-03-09 14:24:20 +0000

    using nesting components method to pass props dowm from child component and into parent component

---
+++ my-app/src/components/NestingComponents.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import UserMessage from './UserMessage'
+import UserData from './UserData'
+
+class NestingComponents extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoaded: true,
+            isLoggedIn: true,
+        }
+    }
+    render() {
+        return (
+            <div> 
+                <UserData isLoaded={this.state.isLoaded}/>
+                <UserMessage isLoggedIn={this.state.isLoggedIn}/>
+            </div>
+        )
+    }
+}
+ // UserMessage (in the div) is passing the props down from the child component (UserMessage)
+ // Userdatat (in the div) is passing props down from the child component (UserData)
+
+export default NestingComponents
